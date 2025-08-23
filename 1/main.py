354c7bf84b7a5f3d1d12b5f7420e068d8b0fe855
Dinkovski <Stefan.japi@gmail.com>
2025-08-23 22:21:11 +0200

    broj dva upload

---
+++ 1/main.py
@@ -0,0 +1,22 @@
+# moja prva skripta
+print("Hello World!")
+
+name = "Dinkovski"
+print(name + " je car")
+
+# age, height
+age=35
+height=175
+print(age,height)
+
+# bool - boolean - true or false
+
+#vezba
+program_name = "Python"
+version = 3.13
+is_new_program = True
+
+
+
+
+
---
+++ 1/vezba.py
@@ -0,0 +1,15 @@
+#vezba
+program_name = "Python"
+version = 3.13
+is_new_program = True
+
+
+#list -> array, "Harry Potter 1", "LOTR collections"
+books = ["Harry Potter 1", "LOTR collection"]
+
+
+print(program_name, version, is_new_program)
+books[0] = "Pragmatic Programmer"
+print(books)
+
+
